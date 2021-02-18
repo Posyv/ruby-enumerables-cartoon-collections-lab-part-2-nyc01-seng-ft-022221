e0cb7afd57380edd1805f0bfd2d8a425db52bc17
Posy Natalie Vogt <nataliepvogt@gmail.com>
2021-02-18 17:11:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def square_array(array)
   array.map do |element|
     element * element
@@ -22,10 +23,10 @@ def long_planeteer_calls(planeteer_calls)
 end
 
 def find_valid_calls(planeteer_calls)
+  binding.pry
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   if planeteer_calls.any?  {|equalnames| = valid_calls}
   end
-  equalnames
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above
   # Return the first valid call found, or return nil if no valid calls are found
 end
